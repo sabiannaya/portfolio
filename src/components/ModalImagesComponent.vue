e35935fc5bfce6f5b21fa0e1257ec225b0b79cec
sabiannaya <sabiannaya@gmail.com>
2025-01-23 00:13:16 +0700

    feat: hover on button chore: removes all debug code

--- src/components/ModalImagesComponent.vue
+++ src/components/ModalImagesComponent.vue
@@ -22,7 +22,6 @@ const nextImage = () => {
   } else {
     currentImage.value = imageArray.value[nextIndex]
   }
-  console.log(currentImage.value.orientation)
 }
 
 const prevImage = () => {
@@ -35,22 +34,22 @@ const prevImage = () => {
 }
 </script>
 <template>
-  <div class="h-screen w-screen fixed bg-red-500 z-10" style="background-color: rgb(0, 0, 0, 0.5)">
+  <div class="h-screen w-screen fixed z-10" style="background-color: rgb(0, 0, 0, 0.5)">
     <div class="flex items-center justify-center w-full h-full">
       <div class="flex flex-col">
         <span class="w-full absolute top-16 left-[80vw]" @click.prevent="emit('closeImageModal')">
-          <i class="fas fa-times fa-2x text-white cursor-pointer"></i>
+          <i class="fas fa-times fa-3x text-gray-200 hover:text-white cursor-pointer"></i>
         </span>
         <span class="absolute left-5 sm:left-10 md:left-20 top-1/2 z-20" @click.prevent="prevImage">
-          <i class="fas fa-chevron-left fa-2x text-white cursor-pointer"></i>
+          <i class="fas fa-chevron-left fa-3x text-gray-200 hover:text-white cursor-pointer"></i>
         </span>
         <span class="absolute right-5 sm:right-10 md:right-20 top-1/2 z-20" @click.prevent="nextImage">
-          <i class="fas fa-chevron-right fa-2x text-white cursor-pointer"></i>
+          <i class="fas fa-chevron-right fa-3x text-gray-200 hover:text-white cursor-pointer"></i>
         </span>
         <img
           :src="currentImage.path"
           alt="image"
-          class="bg-red-500 object-contain"
+          class="object-contain"
           :class="{
             'h-[35vh] md:h-[65vh] w-[70rem] rotate-90 md:rotate-0': currentImage.orientation === 'landscape',
             'h-[35vh] md:h-[65vh] w-[70rem] rotate-90 md:rotate-0': currentImage.orientation === 'even',
